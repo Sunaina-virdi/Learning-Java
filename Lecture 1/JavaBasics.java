b80afcf799a3a312432e0bce40c87de69a646f97
Sunaina <122337517+Sunaina-virdi@users.noreply.github.com>
2024-07-22 15:09:57 +0530

    Add files via upload

---
+++ Lecture 1/JavaBasics.java
@@ -0,0 +1,59 @@
+package Lecture1;
+public class JavaBasics {
+    public static void main(String[] args){
+//  OUTPUT
+        System.out.println("hello world !!");
+        System.out.println("hello world !!");
+/* 
+VARIABLES DEFINE
+         int a = 10;
+         int b = 5;
+         System.out.println(a);
+         System.out.println(b);
+         String name = "Sunaina";
+         System.out.println(name);
+
+         a = 50; //value changed
+         System.out.println(a);
+*/
+/* 
+DATATYPES
+      PRIMITIVE                      NON-PRIMITIVE
+        -> byte      1byte           -> String
+        -> short     2byte           -> Array
+        -> char      2byte           -> Class
+        -> boolean   1byte           -> Object
+        -> int       4byte           -> Interface
+        -> long      8byte
+        -> float     4byte
+        -> double    8byte
+*/
+/*
+BYTE 
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+        boolean var = false;
+        int number = 25;
+        short n = 240;
+        System.out.println(var);
+        System.out.println(number);
+        System.out.println(n);
+*/
+/*Sum
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+ */
+    }
+}
+
+
+    
+    
+    
+    
+
+    
