905b412ba782dee2bbf3e95c4537c2c5a212751c
Sunaina <122337517+Sunaina-virdi@users.noreply.github.com>
2024-07-31 21:33:56 +0530

    Add files via upload

---
+++ Lecture 2/Loops.java
@@ -0,0 +1,14 @@
+package Lecture2;
+
+public class Loops {
+    public static void main(String[] args){
+// while loop
+        int counter = 0;
+        while(counter <= 10){
+            System.out.println(counter);
+            counter++;
+        }
+// sum of natural no.
+        
+    }
+}
